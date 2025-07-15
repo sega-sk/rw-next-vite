83202931204e886bc30fb296cbefd5a534bb9563
SeGa <sega@dealertower.com>
2025-07-15 22:39:51 +0300

    fix

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,5 +1,5 @@
-import { Dashboard } from '../../components/Dashboard'
-import { ProtectedRoute } from '../../components/ProtectedRoute'
+import { Dashboard } from '../../../components/Dashboard'
+import { ProtectedRoute } from '../../../components/ProtectedRoute'
 
 export default function Page() {
   return (
